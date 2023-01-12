617a3d16314460227a69f0388bd19b8e97e906b0
AntonTeslia <teslyaanton@yandex.ru>
2023-01-13 00:33:14 +0300

    feat: добавить классы user и film

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@Data
+public class Film {
+    private int id;
+    private String name;
+    private String description;
+    private Instant releaseDate;
+    private Duration duration;
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.filmorate.model;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private int id;
+    private String email;
+    private String login;
+    private String name;
+    private LocalDateTime birthday;
+}
